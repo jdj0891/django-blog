3d79d7ea64c7d418d755b8320a075fd8475346fa
Jason Jenkins <jdj0891@yahoo.com>
2021-03-03 09:59:29 -0800

    Fixed Testing Bug

--- blogging/urls.py
+++ blogging/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include  # <-- Make sure you have both of these imports.
+from django.urls import path
+from blogging.views import stub_view, list_view
 
 urlpatterns = [
-    path('', include('blogging.urls')),
-    path('polling/', include('polling.urls')),  # <-- Add this
-    path('admin/', admin.site.urls),
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', list_view, name="blog_detail"),
 ]
