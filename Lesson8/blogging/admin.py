87e05e5945f0f94f3b1ca8daafa688b451733ca0
Jason Jenkins <jdj0891@yahoo.com>
2021-03-04 09:58:20 -0800

    Create lesson 8 sub-directory

---
+++ Lesson8/blogging/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.post.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('post',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
